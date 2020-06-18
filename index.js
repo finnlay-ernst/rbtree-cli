e48dce773c18f138a7404501405290248328f181
Finnlay Ernst <fernst14@gmail.com>
2020-06-18 16:21:35 +1000

    Continous polling and multiple operations

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const RBNode = require('./src/rbtree.js');
 const inquirer = require('inquirer');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '>',
+});
 const chalk = require('chalk');
+const { read } = require('fs');
 
 let root;
 const log = console.log;
-inquirer.prompt([
-    {
-        name: 'insertNum',
-        message: "Enter a number to be inserted:",
-    },
-])
-.then(answers => {
-    if (isNaN(answers.insertNum)) {
-        throw 'Entered value not a number.';
-    }
 
-    (root) ? root.insert(answers.insertNum) : root = new RBNode(answers.insertNum)    
-    if(root) {
-        log(`Root value is`);
+log (`Use ${chalk.blueBright('insert _')} or ${chalk.blueBright('delete _')} to modify tree \nInput should be a number\n`);
+readline.prompt(); 
+try {
+    readline.on ('line', (input) => {
+        let inputs = input.toLowerCase().split(" ");
+        if (inputs.length !== 2){
+            log('Error: Invalid input');
+        }
+        // else if (isNaN(input[1])){
+        //     throw 'Error: Input not a number'
+        // }
+
+        switch (inputs[0]){
+            case 'insert': {
+                (root) ? root.insert(inputs[1]) : root = new RBNode(inputs[1]);
+                break;
+            }
+            case 'delete': {
+                if (root){
+                    root.delete(inputs[1]);
+                }
+                break;
+            }
+            default: log('Error: Unrecognised command');
+        }
+
+            
+        //TODO: Print out the tree
         //Set the styling funtion approriately 
-        let logStyling = (root.colour == "BLACK") ? chalk.bold.black : chalk.bold.red;
-        log(logStyling(root.value)); 
-    }
-    else {
-        log(`Root not defined`);
-    }        
-})
-.catch(error => {console.error(error)});
+        if (root){
+            let logStyling = (root.colour == "BLACK") ? chalk.bold.black : chalk.bold.red;
+            log(logStyling(root.value));
+        }
+        readline.prompt(); 
+    });     
+} catch (error) {
+    console.error(error);
+}
+
