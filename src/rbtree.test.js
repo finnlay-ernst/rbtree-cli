5c903cf251d851b0cc008fa42f17a34a0bd911b6
Finnlay Ernst <fernst14@gmail.com>
2020-06-19 15:48:49 +1000

    Added test stubs

---
+++ src/rbtree.test.js
@@ -0,0 +1,110 @@
+const RBNode = require('./rbtree.js');
+
+
+test ('Example test', () => {
+    expect(1+2).toBe(3);
+});
+
+describe('Insert tests:', () => {
+    //TODO: complete test list
+    describe('Binary tree properites:', () => {
+        test('Unique element is inserted', () => {
+            //TODO
+            expect(true).toBe(true);
+        });
+        test('Duplicate element not inserted', () => {
+            //TODO
+            expect(true).toBe(true);
+        });
+        test('Elements < root go in left subtree', () => {
+            //TODO
+            expect(true).toBe(true);
+        });
+        test('Elements > root go in right subtree', () => {
+            //TODO
+            expect(true).toBe(true);
+        });
+        test('Non integer elements throw error', () => {
+            //TODO
+            expect(true).toBe(false);
+        });    
+    });
+    describe('Red black tree properites:', () => {
+    
+    });    
+});
+
+describe('Delete tests:', () => {
+    //TODO: complete test list
+    describe('Binary tree properites:', () => {
+        test('Unique element is inserted', () => {
+            //TODO
+            expect(true).toBe(true);
+        });
+        test('Duplicate element not inserted', () => {
+            //TODO
+            expect(true).toBe(true);
+        });
+        test('Elements < root go in left subtree', () => {
+            //TODO
+            expect(true).toBe(true);
+        });
+        test('Elements > root go in right subtree', () => {
+            //TODO
+            expect(true).toBe(true);
+        });
+        test('Non integer elements throw error', () => {
+            //TODO
+            expect(true).toBe(false);
+        });    
+    });
+    describe('Red black tree properites:', () => {
+    
+    });    
+});
+
+describe('Find tests:', () => {
+    //TODO: complete test list    
+    test('Unique element is inserted', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+    test('Duplicate element not inserted', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+    test('Elements < root go in left subtree', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+    test('Elements > root go in right subtree', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+    test('Non integer elements throw error', () => {
+        //TODO
+        expect(true).toBe(false);
+    });      
+});
+
+describe('Flatten tests:', () => {
+    //TODO: complete test list  
+});
+
+describe('Collapse tests:', () => {
+    //TODO: complete test list
+});
+
+describe('Rotation tests:', () => {
+    //TODO: complete test list
+    describe('Left rotate tests:', () => {
+  
+    });
+    describe('Right rotate tests:', () => {
+    
+    });    
+});
+
+describe('Test red black properties:', () => {
+    //TODO: complete test list    
+});
