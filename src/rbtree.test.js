2c3ba51a8ba695eaa520d297036b6280037ea374
Finnlay Ernst <fernst14@gmail.com>
2020-06-19 16:31:32 +1000

    Iterated most binary tree tests

--- src/rbtree.test.js
+++ src/rbtree.test.js
@@ -5,6 +5,13 @@ test ('Example test', () => {
     expect(1+2).toBe(3);
 });
 
+describe('General input processing and exception handling tests:', () => {
+    test('Non-integer inputs throw error', () => {
+        //TODO
+        expect(true).toBe(true);
+    }); 
+});
+
 describe('Insert tests:', () => {
     //TODO: complete test list
     describe('Binary tree properites:', () => {
@@ -12,7 +19,7 @@ describe('Insert tests:', () => {
             //TODO
             expect(true).toBe(true);
         });
-        test('Duplicate element not inserted', () => {
+        test('Duplicate elements are not inserted', () => {
             //TODO
             expect(true).toBe(true);
         });
@@ -23,11 +30,7 @@ describe('Insert tests:', () => {
         test('Elements > root go in right subtree', () => {
             //TODO
             expect(true).toBe(true);
-        });
-        test('Non integer elements throw error', () => {
-            //TODO
-            expect(true).toBe(false);
-        });    
+        });   
     });
     describe('Red black tree properites:', () => {
     
@@ -37,71 +40,112 @@ describe('Insert tests:', () => {
 describe('Delete tests:', () => {
     //TODO: complete test list
     describe('Binary tree properites:', () => {
-        test('Unique element is inserted', () => {
+        test('Existing element is deleted', () => {
             //TODO
             expect(true).toBe(true);
         });
-        test('Duplicate element not inserted', () => {
+        test('Deleting only element throws error', () => {
             //TODO
             expect(true).toBe(true);
         });
-        test('Elements < root go in left subtree', () => {
+        test('Deleting non-existant element hasd no effect', () => {
             //TODO
             expect(true).toBe(true);
         });
-        test('Elements > root go in right subtree', () => {
+        test('Deleting element does not leave subtrees detatched', () => {
             //TODO
             expect(true).toBe(true);
-        });
-        test('Non integer elements throw error', () => {
-            //TODO
-            expect(true).toBe(false);
-        });    
+        });        
     });
     describe('Red black tree properites:', () => {
     
     });    
 });
 
-describe('Find tests:', () => {
-    //TODO: complete test list    
-    test('Unique element is inserted', () => {
+describe('Size tests:', () => { 
+    test('Size of single node tree is 1', () => {
         //TODO
         expect(true).toBe(true);
     });
-    test('Duplicate element not inserted', () => {
+    test('Adding nodes increases size appropriately', () => {
         //TODO
         expect(true).toBe(true);
     });
-    test('Elements < root go in left subtree', () => {
+    test('Removing nodes decreases size appropriately', () => {
         //TODO
         expect(true).toBe(true);
     });
-    test('Elements > root go in right subtree', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
-    test('Non integer elements throw error', () => {
-        //TODO
-        expect(true).toBe(false);
-    });      
 });
 
-describe('Flatten tests:', () => {
-    //TODO: complete test list  
+describe('Depth tests:', () => { 
+    test('Depth of single node tree is 1', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+    test('Depth changes when nodes are removed (if it should)', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+    test('Depth is always 2log2(n + 1)', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+});
+
+describe('Find tests:', () => {  
+    test('Nodes in left subtree are returned', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+    test('Nodes in right subtree are returned', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+    test('Returns null for non-existant elements', () => {
+        //TODO
+        expect(true).toBe(true);
+    });
+});
+
+describe('Flatten tests:', () => { 
+    test('Array length is always equal to tree size', () => {
+        //TODO
+        expect(true).toBe(true);
+    }); 
+    test('Flattens appropriately', () => {
+        //TODO
+        expect(true).toBe(true);
+    }); 
 });
 
 describe('Collapse tests:', () => {
-    //TODO: complete test list
+    test('Array length is always equal to tree size', () => {
+        //TODO
+        expect(true).toBe(true);
+    }); 
+    test('Collapses appropriately', () => {
+        //TODO
+        expect(true).toBe(true);
+    }); 
 });
 
 describe('Rotation tests:', () => {
     //TODO: complete test list
+    test('Calling rotate on node with no childeren has no effect', () => {
+        //TODO
+        expect(true).toBe(true);
+    }); 
     describe('Left rotate tests:', () => {
-  
+        test('Right child is now parent', () => {
+            //TODO
+            expect(true).toBe(true);
+        }); 
     });
     describe('Right rotate tests:', () => {
-    
+        test('Left child is now parent', () => {
+            //TODO
+            expect(true).toBe(true);
+        }); 
     });    
 });
 
