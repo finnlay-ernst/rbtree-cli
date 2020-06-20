514db92c79737f9d6a2d8a1fbacb684068e202d0
Finnlay Ernst <fernst14@gmail.com>
2020-06-20 15:31:40 +1000

    Fixed up mutation issues a bit (testTree now initilised before each test)

--- src/rbtree.js
+++ src/rbtree.js
@@ -1,83 +1,81 @@
 //Colours that a node can be
 const colours = {
-    Red: "RED",
-    Black: "BLACK",
+	Red: "RED",
+	Black: "BLACK",
 };
 
 //We don't ever want to change this object
 Object.freeze(colours);
-
 module.exports = class RBNode {
-    //Default attributes
-    colour = colours.Black; 
-    parent = null;
-    left = null
-    right = null;
-    constructor(value) {
-        this.value = value;
-    }
+	//Default attributes
+	colour = colours.Black;
+	parent = null;
+	left = null;
+	right = null;
+	constructor(value) {
+		this.value = value;
+	}
 
-    /*
+	/*
         @param Value to be inserted    
     */
-    insert(val) {
-        //TODO: implement insertion     
-    }
+	insert(val) {
+		//TODO: implement insertion
+	}
 
-    /*
+	/*
         @param Value to be deleted 
     */
-    delete(val) {
-        //TODO: implement deletion
-    }
+	delete(val) {
+		//TODO: implement deletion
+	}
 
-    /*
+	/*
         @param Value to be found 
         @return The object representing that node or null if not found
     */
-    find(val) {
-        //TODO: implement find
-        return null;
-    }
+	find(val) {
+		//TODO: implement find
+		return null;
+	}
 
-    /* 
+	/* 
         @return Total number of nodes in the tree (all subnodes of this node)
     */
-    size(){
-        //TODO: implement size
-        return 1;
-    }
+	size() {
+		//TODO: implement size
+		return 1;
+	}
 
-    /*
+	/*
         @return The number of levels in the tree
     */
-    depth(){
-        //TODO: implement depth
-        return 0;
-    }
+	depth() {
+		//TODO: implement depth
+		return 0;
+	}
 
-    /*        
+	/*        
         @return An array of all values in the tree left to right
     */
-    flatten(){
-        //TODO: implement flatten
-        return [];
-    }
+	flatten() {
+		//TODO: implement flatten
+		return [];
+	}
 
-    /*        
+	/*        
         @return An array of all nodes in the tree starting from center (so that reinserting would not require rotation, good for serilization)
     */
-    collapse(){
-        //TODO: implement collapse
-        return [];
-    }
+	collapse() {
+		//TODO: implement collapse
+		return [];
+	}
 
-    leftRotate(){
-        //TODO: implement leftRotate
-    }
+	leftRotate() {
+		//TODO: implement leftRotate
+	}
 
-    rightRotate(){
-        //TODO: implement rightRotate
-    }
-
+	rightRotate() {
+		//TODO: implement rightRotate
+	}
 }
--- src/rbtree.test.js
+++ src/rbtree.test.js
@@ -1,239 +1,226 @@
-const RBNode = require('./rbtree.js');
+const RBNode = require("./rbtree.js");
 
-
-test ('Example test', () => {
-    expect(1+2).toBe(3);
+test("Example test", () => {
+	expect(1 + 2).toBe(3);
 });
 
-/*
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-    @param min: integer minimum 
-           max: integer maximum
-    @return A random integer between min and max (including min but not max)
-*/
-// function getRandomInt(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min)) + min; 
-// }
+let testTree = null;
 
-
-
-// const randRoot = getRandomInt(50, 100);
-// const randomTree = new RBNode(randRoot);
-
-const manualTree = new RBNode(50);
-
-describe('General input processing and exception handling tests:', () => {
-    test('Non-integer inputs throw error', () => {
-        //Check 2 data types for the manual tree
-        expect(() => {manualTree.insert('ABC')}).toThrow();
-        expect(() => {manualTree.delete('ABC')}).toThrow();
-        expect(() => {manualTree.find('ABC')}).toThrow();
-        expect(() => {manualTree.insert([1, 5, 7])}).toThrow();
-        expect(() => {manualTree.delete([1, 5, 7])}).toThrow();
-        expect(() => {manualTree.find([1, 5, 7])}).toThrow();
-
-        // //Check 2 data types for the random tree
-        // expect(() => {randomTree.insert('ABC')}).toThrow();
-        // expect(() => {randomTree.delete('ABC')}).toThrow();
-        // expect(() => {randomTree.find('ABC')}).toThrow();
-        // expect(() => {randomTree.insert([1, 5, 7])}).toThrow();
-        // expect(() => {randomTree.delete([1, 5, 7])}).toThrow();
-        // expect(() => {randomTree.find([1, 5, 7])}).toThrow();
-    }); 
+beforeEach(() => {
+	//Before every test instatiate tree to this
+	testTree = new RBNode(50);
 });
 
-describe('Insert tests:', () => {
-    describe('Binary tree properites:', () => {
-        const prevSize = manualTree.size(); 
-        manualTree.insert(25);        
-        manualTree.insert(75);
-        test('Unique element is inserted', () => {     
-            const find25 = manualTree.find(25);   
-            const find75 = manualTree.find(75);
-            const newSize = manualTree.size();
-            
-            expect(find25).not.toBeNull();
-            expect(find75).not.toBeNull();
-            expect(newSize).toEqual(prevSize+2);
-        });
-        test('Duplicate elements are not inserted', () => {
-            const preDupSize = manualTree.size(); 
-            manualTree.insert(50);
-            const postDupSize = manualTree.size();
-
-            expect(postDupSize).toEqual(preDupSize);
-        });
-        test('Elements < root go in left subtree', () => {           
-            expect(manualTree.left).not.toBeNull();
-            expect(manualTree.left.value).toEqual(25);                
-        });
-        test('Elements > root go in right subtree', () => {        
-            expect(manualTree.right).not.toBeNull();
-            expect(manualTree.right.value).toEqual(75);                
-        });   
-    });
-    describe('Red black tree properites:', () => {
-        //TODO
-    });    
+describe("General input processing and exception handling tests:", () => {
+	test("Non-integer inputs throw error", () => {
+		//Check 2 data types for the manual tree
+		expect(() => {
+			testTree.insert("ABC");
+		}).toThrow();
+		expect(() => {
+			testTree.delete("ABC");
+		}).toThrow();
+		expect(() => {
+			testTree.find("ABC");
+		}).toThrow();
+		expect(() => {
+			testTree.insert([1, 5, 7]);
+		}).toThrow();
+		expect(() => {
+			testTree.delete([1, 5, 7]);
+		}).toThrow();
+		expect(() => {
+			testTree.find([1, 5, 7]);
+		}).toThrow();
+	});
 });
 
-describe('Delete tests:', () => {
-    describe('Binary tree properites:', () => {
-        test('Existing element is deleted', () => {
-            const preDelSize = manualTree.size();
-            let find25 = manualTree.find(25);
-            
-            expect(find25).not.toBeNull();
-            manualTree.delete(25);
-            
-            const postDelSize = manualTree.size();
-            find25 = manualTree.find(25);
+describe("Insert tests:", () => {
+	describe("Binary tree properites:", () => {
+		let prevSize;
+		beforeEach(() => {
+			//Setup for tests in this describe block
+			prevSize = testTree.size();
+			testTree.insert(25);
+			testTree.insert(75);
+		});
+		test("Unique element is inserted", () => {
+			expect(testTree.find(25)).not.toBeNull();
+			expect(testTree.find(75)).not.toBeNull();
+			expect(testTree.size()).toEqual(prevSize + 2);
+		});
+		test("Duplicate elements are not inserted", () => {
+			const preDupSize = testTree.size();
+			testTree.insert(50);
 
-            expect(find25).toBeNull();
-            expect(postDelSize).toEqual(preDelSize-1);
-        });
-        test('Deleting only element throws error', () => {
-            const singleNodeTree = new RBNode(100);        
-            expect(() => {singleNodeTree.delete(10)}).toThrow();
-        });
-        test('Deleting non-existant element hasd no effect', () => {
-            const preDelManTree = manualTree;
-            manualTree.delete(10000);
-            //toEqual checks all the properties are the same while toBe checks that objects ARE the same
-            expect(manualTree).toEqual(preDelManTree); 
-        });
-        test('Deleting element does not leave subtrees detatched', () => {
-            //Insert 2 elements because rb tree will rotate when imbalanced
-            manualTree.insert(100);
-            manualTree.insert(125);
-            manualTree.delete(100);            
-            expect(manualTree.right).not.toBeNull();
-            expect(manualTree.right.value).toEqual(125);
-        });        
-    });
-    describe('Red black tree properites:', () => {
-        //TODO
-    });    
+			expect(testTree.size()).toEqual(preDupSize);
+		});
+		test("Elements < root go in left subtree", () => {
+			expect(testTree.left).not.toBeNull();
+			expect(testTree.left.value).toEqual(25);
+		});
+		test("Elements > root go in right subtree", () => {
+			expect(testTree.right).not.toBeNull();
+			expect(testTree.right.value).toEqual(75);
+		});
+	});
+	describe("Red black tree properites:", () => {
+		//TODO
+	});
 });
 
-describe('Size tests:', () => { 
-    const multiNodeTree = new RBNode(50);
-    multiNodeTree.insert(25);
-    multiNodeTree.insert(75);
-    multiNodeTree.insert(125);
-    test('Size of single node tree is 1', () => {
-        const singleNodeTree = new RBNode(100);
-        const size = singleNodeTree.size();
-        expect(size).toEqual(1);
-    });
-    test('Adding nodes increases size appropriately', () => {
-        const size = multiNodeTree.size();
-        expect(size).toEqual(4);
-    });
-    test('Removing nodes decreases size appropriately', () => {
-        multiNodeTree.delete(75);
-        let size = multiNodeTree.size();
-        expect(size).toBe(3);
-        
-        multiNodeTree.delete(25);
-        size = multiNodeTree.size();
-        expect(size).toBe(2);
-    });
+describe("Delete tests:", () => {
+	describe("Binary tree properites:", () => {
+		beforeEach(() => {
+			//Setup for tests in this describe block
+			testTree.insert(25);
+			testTree.insert(75);
+		});
+		test("Existing element is deleted", () => {
+			const preDelSize = testTree.size();
+
+			expect(testTree.find(25)).not.toBeNull();
+			testTree.delete(25);
+
+			expect(testTree.find(25)).toBeNull();
+			expect(testTree.size()).toEqual(preDelSize - 1);
+		});
+		test("Deleting only element throws error", () => {
+			const singleNodeTree = new RBNode(100);
+			expect(() => {
+				singleNodeTree.delete(100);
+			}).toThrow();
+		});
+		test("Deleting non-existant element hasd no effect", () => {
+			const preDelManTree = testTree;
+			testTree.delete(10000);
+			//toEqual checks all the properties are the same while toBe checks that objects ARE the same
+			expect(testTree).toEqual(preDelManTree);
+		});
+		test("Deleting element does not leave subtrees detatched", () => {
+			testTree.insert(100);
+			testTree.delete(75);
+			expect(testTree.right).not.toBeNull();
+			expect(testTree.right.value).toEqual(100);
+		});
+	});
+	describe("Red black tree properites:", () => {
+		//TODO
+	});
 });
 
-describe('Depth tests:', () => { 
-    test('Depth of single node tree is 1', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
-    test('Depth changes when nodes are removed (if it should)', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
-    test('Depth is always 2log2(n + 1)', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
+describe("Size tests:", () => {
+	test("Size of single node tree is 1", () => {
+		expect(testTree.size()).toEqual(1);
+	});
+	test("Adding nodes increases size appropriately", () => {
+		testTree.insert(25);
+		testTree.insert(75);
+		expect(testTree.size()).toEqual(3);
+	});
+	test("Removing nodes decreases size appropriately", () => {
+		testTree.insert(25);
+		testTree.insert(75);
+		expect(testTree.size()).toEqual(3);
+
+		testTree.delete(25);
+		expect(testTree.size()).toEqual(2);
+		testTree.delete(75);
+		expect(testTree.size()).toEqual(1);
+	});
 });
 
-describe('Find tests:', () => {  
-    test('Nodes in left subtree are returned', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
-    test('Nodes in right subtree are returned', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
-    test('Returns null for non-existant elements', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
+describe("Depth tests:", () => {
+	test("Depth of single node tree is 1", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("Depth changes when nodes are removed (if it should)", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("Depth is always 2log2(n + 1)", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
 });
 
-describe('Flatten tests:', () => { 
-    test('Array length is always equal to tree size', () => {
-        //TODO
-        expect(true).toBe(true);
-    }); 
-    test('Flattens appropriately', () => {
-        //TODO
-        expect(true).toBe(true);
-    }); 
+describe("Find tests:", () => {
+	test("Nodes in left subtree are returned", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("Nodes in right subtree are returned", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("Returns null for non-existant elements", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
 });
 
-describe('Collapse tests:', () => {
-    test('Array length is always equal to tree size', () => {
-        //TODO
-        expect(true).toBe(true);
-    }); 
-    test('Collapses appropriately', () => {
-        //TODO
-        expect(true).toBe(true);
-    }); 
+describe("Flatten tests:", () => {
+	test("Array length is always equal to tree size", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("Flattens appropriately", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
 });
 
-describe('Rotation tests:', () => {
-    //TODO: complete test list
-    test('Calling rotate on node with no childeren has no effect', () => {
-        //TODO
-        expect(true).toBe(true);
-    }); 
-    describe('Left rotate tests:', () => {
-        test('Right child is now parent', () => {
-            //TODO
-            expect(true).toBe(true);
-        }); 
-    });
-    describe('Right rotate tests:', () => {
-        test('Left child is now parent', () => {
-            //TODO
-            expect(true).toBe(true);
-        }); 
-    });    
+describe("Collapse tests:", () => {
+	test("Array length is always equal to tree size", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("Collapses appropriately", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
 });
 
-describe('Test red black properties:', () => { 
-    test('Root node is black in a single node tree', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
-    test('Root node is black in a multi-node tree', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
-    test('Null leaves are black', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
-    test('All red nodes have black parents', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
-    test('All paths to leaves have equivelent black node count', () => {
-        //TODO
-        expect(true).toBe(true);
-    });
+describe("Rotation tests:", () => {
+	//TODO: complete test list
+	test("Calling rotate on node with no childeren has no effect", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	describe("Left rotate tests:", () => {
+		test("Right child is now parent", () => {
+			//TODO
+			expect(true).toBe(true);
+		});
+	});
+	describe("Right rotate tests:", () => {
+		test("Left child is now parent", () => {
+			//TODO
+			expect(true).toBe(true);
+		});
+	});
+});
+
+describe("Test red black properties:", () => {
+	test("Root node is black in a single node tree", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("Root node is black in a multi-node tree", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("Null leaves are black", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("All red nodes have black parents", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
+	test("All paths to leaves have equivelent black node count", () => {
+		//TODO
+		expect(true).toBe(true);
+	});
 });
