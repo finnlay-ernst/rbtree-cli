7e0db772420be4c77c0393c45079e745eca0f24c
Finnlay Ernst <fernst14@gmail.com>
2020-06-23 12:04:31 +1000

    Changed class structure, switching to a loop based implementation

--- src/rbtree.js
+++ src/rbtree.js
@@ -1,5 +1,3 @@
-const { black } = require("chalk");
-
 //Colours that a node can be
 const colours = {
 	Red: "RED",
@@ -21,96 +19,19 @@ class RBNode {
 	constructor(value) {
 		this.value = value;
 	}
-
-	/*
-		@param Value to be inserted 
-		@return The inserted node
-    */
-	insert(val) {
-		//TODO: implement insert
-		return null;
-	}
-
-	/*
-		@param Value to be deleted 
-		@return The node that replaced the deleted node 
-    */
-	delete(val) {
-		//TODO: implement deletion
-		return null;
-	}
-
-	/*
-        @param Value to be found 
-        @return The object representing that node or null if not found
-    */
-	find(val) {
-		//TODO: implement find
-		return null;
-	}
-
-	/* 
-        @return Total number of nodes in the tree (all subnodes of this node)
-    */
-	size() {
-		//TODO: implement size
-		return 1;
-	}
-
-	/*
-        @return The number of levels in the tree
-    */
-	depth() {
-		//TODO: implement depth
-		return 0;
-	}
-
-	/*        
-        @return An array of all values in the tree left to right
-    */
-	flatten() {
-		//TODO: implement flatten
-		return [];
-	}
-
-	/*        
-        @return An array of all nodes in the tree starting from center (so that reinserting would not require rotation, good for serilization)
-    */
-	collapse() {
-		//TODO: implement collapse
-		return [];
-	}
-
-	/*
-		@return The minimum node in this subtree
-	*/
-	min(){
-		//TODO: implement min
-	}
-
-
-	fixup(){
-		//TODO: implement the 3 cases for fixup
-	}
-
-	leftRotate() {
-		//TODO: implement leftRotate
-	}
-
-	rightRotate() {
-		//TODO: implement rightRotate
-	}
 }
 
 /*
-    Need a seperate class for tree as the root will often need to change to maintain red black properties.
-    Input validation will also be performed in this class.
+    Tree class for performing operations on the tree
 */
 class RBTree {
 	constructor() {
 		this.root = null;	
 	}
 
+	/*
+		@param Value to be inserted 
+    */
 	insert(val) {
 		if (this.root === null) this.root = new RBNode(val);
 		else this.root.insert(val);
@@ -160,6 +81,29 @@ class RBTree {
 	*/
 	collapse() {
 		return this.root !== null ? this.root.collapse() : [];
+	}
+
+	/*
+		@return The minimum node in this subtree
+	*/
+	min(){
+		//TODO: implement min
+	}
+
+	insertFixup(){
+		//TODO: implement the 3 cases for fixup
+	}
+
+	deleteFixup(){
+		//TODO: implement the 4 cases for fixup
+	}
+
+	leftRotate() {
+		//TODO: implement leftRotate
+	}
+
+	rightRotate() {
+		//TODO: implement rightRotate
 	}
 }
 
